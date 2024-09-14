2152ed4f60207c633630abf75306b84f420a8e9a
shanmaqbool7426 <67848440+shanmaqbool7426@users.noreply.github.com>
2024-09-14 15:07:05 -0700

    sd

--- app/blog/[[...slug]]/page.jsx
+++ app/blog/[[...slug]]/page.jsx
@@ -4,7 +4,7 @@ import BlogModule from "@/modules/blogs";
 
 export default function Blog({ params, searchParams }) {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div>Loading....</div>}>
       <BlogModule params={params} searchParams={searchParams} />
     </Suspense>
   );
