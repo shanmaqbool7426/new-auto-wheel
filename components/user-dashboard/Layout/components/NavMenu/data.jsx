69b499b36df526e5751db3672372ec8b1cc015e4
Ghulam Mohiuddin <ghulam.mohiuddin@consultancyoutfit.co.uk>
2024-09-29 13:02:48 +0500

    Added #User Dashboard > Sidebar Menu

---
+++ components/user-dashboard/Layout/components/NavMenu/data.jsx
@@ -0,0 +1,41 @@
+import {
+  IconDashboard,
+  IconHeart,
+  IconInventory,
+  IconProfileSettings,
+  IconSocial,
+  IconReviews,
+} from './icons';
+
+export const data = [
+  {
+    label: 'Dashboard',
+    href: '/user',
+    icon: IconDashboard,
+  },
+  {
+    label: 'Inventory',
+    href: '/user/inventory',
+    icon: IconInventory,
+  },
+  {
+    label: 'Favorite',
+    href: '/user/favorite',
+    icon: IconHeart,
+  },
+  {
+    label: 'Social',
+    href: '/user/social',
+    icon: IconSocial,
+  },
+  {
+    label: 'User Reviews',
+    href: '/user/reviews',
+    icon: IconReviews,
+  },
+  {
+    label: 'Profile Settings',
+    href: '/user/profile-settings',
+    icon: IconProfileSettings,
+  },
+];
