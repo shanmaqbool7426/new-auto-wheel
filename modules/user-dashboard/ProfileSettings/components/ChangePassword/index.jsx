b20032098ff57f87e0d83eedb803cd27c9173a53
shanmaqbool7426 <67848440+shanmaqbool7426@users.noreply.github.com>
2024-10-02 08:11:56 -0700

    build issues fix

--- modules/user-dashboard/ProfileSettings/components/ChangePassword/index.jsx
+++ modules/user-dashboard/ProfileSettings/components/ChangePassword/index.jsx
@@ -4,7 +4,7 @@ import { Grid, Button, Box } from '@mantine/core';
 import Card from '@/components/user-dashboard/Card';
 import FormField from '@/components/user-dashboard/FormField';
 import useChangePassword from './useChangePassword';
-import buttonStyles from '@/styles/user-dashboard/button.module.css';
+import buttonStyles from '@/styles/user-dashboard/Button.module.css';
 import styles from './ChangePassword.module.css';
 
 export default function ChangePassword() {
