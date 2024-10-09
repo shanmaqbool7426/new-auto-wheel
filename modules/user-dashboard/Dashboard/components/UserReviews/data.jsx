941db2246883ce7bdf50bb77c223b814993acfd6
Ghulam Mohiuddin <ghulam.mohiuddin@consultancyoutfit.co.uk>
2024-10-09 22:31:11 +0500

    Added #Dashboard > User Reviews Card

---
+++ modules/user-dashboard/Dashboard/components/UserReviews/data.jsx
@@ -0,0 +1,29 @@
+export const reviewData = [
+  {
+    id: '6f9sd34969a0f1',
+    title: 'Toyota Corolla Review',
+    modal: '2023 Toyota Corolla XLi VVTi',
+    postedBy: 'Hamza tahir',
+    postedDate: new Date(),
+    rating: 3.5,
+    description: "I've driven my Toyota Corolla 1.8 automatic for more than 70,000 kilometers, and it's been a solid family car. It's really good for...",
+  },
+  {
+    id: '9sd349sdsd69a0lkj',
+    title: 'Toyota Corolla Review',
+    modal: '2023 Toyota Corolla XLi VVTi',
+    postedBy: 'Hamza tahir',
+    postedDate: new Date(),
+    rating: 3.5,
+    description: "I've driven my Toyota Corolla 1.8 automatic for more than 70,000 kilometers, and it's been a solid family car. It's really good for...",
+  },
+  {
+    id: '9sd349sdsd',
+    title: 'Toyota Corolla Review',
+    modal: '2023 Toyota Corolla XLi VVTi',
+    postedBy: 'Hamza tahir',
+    postedDate: new Date(),
+    rating: 3.5,
+    description: "I've driven my Toyota Corolla 1.8 automatic for more than 70,000 kilometers, and it's been a solid family car. It's really good for...",
+  },
+]
--- modules/user-dashboard/Dashboard/components/UserReviews/index.jsx
+++ modules/user-dashboard/Dashboard/components/UserReviews/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Box } from '@mantine/core';
+import dayjs from 'dayjs';
+import { Box, Group, Rating } from '@mantine/core';
 import styles from './UserReviews.module.css';
 import ViewallButton from '../ViewallButton';
+import { reviewData } from './data';
 
 export default function UserReviews() {
   return (
@@ -9,6 +11,33 @@ export default function UserReviews() {
       <Box className={styles.cardHeader}>
         <Box className={styles.cardTitle}>User Reviews</Box>
         <ViewallButton onClick={() => alert('I am clicked')} />
+      </Box>
+      <Box className={styles.cardContent}>
+        <ul className={styles.reviewsList}>
+          {reviewData.map((review) => (
+            <li className={styles.reviewListItem}>
+              <Box className={styles.review}>
+                <Box className={styles.reviewHeader}>
+                  <Box className={styles.reviewTitle}>{review.title}</Box>
+                  <Box className={styles.reviewRating}>
+                    <Group>
+                      <Rating size={'xs'} defaultValue={3} readOnly />
+                      <Box className={styles.postRatingText}>({review.rating}/5)</Box>
+                    </Group>
+                  </Box>
+                </Box>
+                <Box className={styles.reviewMeta}>
+                  <Box className={styles.reviewModal}>{review.modal}</Box>
+                  <Box className={styles.reviewAuthor}>Posted by {review.postedBy} on {dayjs(review.postedDate).format('MMM D, YYYY')}</Box>
+                </Box>
+                <Box className={styles.reviewContent}>
+                  {review.description} <Box className={styles.redText} component='span'>read more</Box>
+                </Box>
+              </Box>
+            </li>
+          ))}
+
+        </ul>
       </Box>
     </Box>
   )
