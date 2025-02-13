59f238b84d88754351ee7f6ca20c8df26d5e282b
wahabansari <wahab0421690@gmail.com>
2025-02-13 17:23:56 +0500

    =listing and layout overflowing changes

--- modules/make-vehicles/index.jsx
+++ modules/make-vehicles/index.jsx
@@ -84,82 +84,101 @@ const MakesVehicles = ({
 
   return (
     <>
-      <section className="find-cars" style={{marginTop:"50px"}}>
-        <Box className="background-search-verlay" mb="220">
-          <div className="container-xl">
-            <div className="row">
-              <div className="col-md-12">
-                <nav className="mt-3">
-                  <ol className="breadcrumb">
+      {/* style={{ marginTop: "50px" }} */}
+      <section className="find-cars">
+        <Box className="background-search-verlay" />
+        <Box className="container-xl" mt={rem(-140)}>
+          <div className="row">
+            <div className="col-md-12">
+              <nav className="mt-3">
+                <ol className="breadcrumb">
                   <li className="breadcrumb-item">
-                      <Anchor href={`/`} tt="capitalize" component={Link}>Home</Anchor>
-                    </li>
-                    <li className="breadcrumb-item">
-                      <Anchor href={`/new/${vehicleType}`} tt="capitalize" component={Link}>New {vehicleType}</Anchor>
-                    </li>
-                    <li className="breadcrumb-item active" aria-current="page">
-                      <Anchor href="#" tt="capitalize">{slugMake}</Anchor>
-                    </li>
-                  </ol>
-                </nav>
-              </div>
-              <div className="col-md-12">
-                <Box className="search-wrapper-card">
-                  <Card
-                    shadow="0px 4px 20px 0px #00000014"
-                    padding="lg"
-                    radius="sm"
-                  >
-                    <Title order={3} mb="md">
-                      {slugMake} {new Date().getFullYear()} Car Models, Prices &
-                      Pictures in Pakistan
-                    </Title>
-                    <div className="row mb-2">
-                      <div className="col-md-3">
-                        <Card shadow="none" h="100%" withBorder py="lg">
-                          <Flex
-                            direction="column"
-                            gap="xs"
-                            justify="center"
-                            align="center"
+                    <Anchor href={`/`} tt="capitalize" component={Link}>
+                      Home
+                    </Anchor>
+                  </li>
+                  <li className="breadcrumb-item">
+                    <Anchor
+                      href={`/new/${vehicleType}`}
+                      tt="capitalize"
+                      component={Link}
+                    >
+                      New {vehicleType}
+                    </Anchor>
+                  </li>
+                  <li className="breadcrumb-item active" aria-current="page">
+                    <Anchor href="#" tt="capitalize">
+                      {slugMake}
+                    </Anchor>
+                  </li>
+                </ol>
+              </nav>
+            </div>
+            <div className="col-md-12">
+              <Box className="search-wrapper-card">
+                <Card
+                  shadow="0px 4px 20px 0px #00000014"
+                  padding="lg"
+                  radius="sm"
+                >
+                  <Title order={3} mb="md">
+                    {slugMake} {new Date().getFullYear()} Car Models, Prices &
+                    Pictures in Pakistan
+                  </Title>
+                  <div className="row mb-2">
+                    <div className="col-md-3">
+                      <Card shadow="none" h="100%" withBorder py="lg">
+                        <Flex
+                          direction="column"
+                          gap="xs"
+                          justify="center"
+                          align="center"
+                        >
+                          <Image
+                            src={matchedMake?.companyImage}
+                            alt="Toyota Logo"
+                            h={100}
+                            w={100}
+                          />
+                          <Title order={5} fw={600} c="#E90808">
+                            {slugMake} Pricelist
+                          </Title>
+                          <Button
+                            href={`/listing/${vehicleType}s/search/-/mk_${slugMake.toLowerCase()}`}
+                            variant="outline"
+                            color="#E90808"
+                            mt="sm"
+                            component={Link}
                           >
-                            <Image
-                              src={matchedMake?.companyImage}
-                              alt="Toyota Logo"
-                              h={100}
-                              w={100}
-                            />
-                            <Title order={5} fw={600} c="#E90808">
-                              {slugMake} Pricelist
-                            </Title>
-                            <Button href={`/listing/${vehicleType}s/search/-/mk_${slugMake.toLowerCase()}`} variant="outline" color="#E90808" mt="sm" component={Link} >
-                              Used {slugMake} Cars for sale
-                            </Button>
-                          </Flex>
-                        </Card>
-                      </div>
-                      <div className="col-md-9">
-                        <Text mb="md">{shortText}</Text>
-                        <Collapse in={isExpanded} transitionDuration={500}>
-                          <Text mb="md">{text.slice(150)}</Text>
-                        </Collapse>
-                        <Button color="red" fw={500} onClick={toggleReadMore}>
-                          {isExpanded ? "Show Less" : "Read More"}
-                        </Button>
-                      </div>
+                            Used {slugMake} Cars for sale
+                          </Button>
+                        </Flex>
+                      </Card>
                     </div>
-                  </Card>
-                </Box>
-              </div>
+                    <div className="col-md-9">
+                      <Text mb="md">{shortText}</Text>
+                      <Collapse in={isExpanded} transitionDuration={500}>
+                        <Text mb="md">{text.slice(150)}</Text>
+                      </Collapse>
+                      <Button color="red" fw={500} onClick={toggleReadMore}>
+                        {isExpanded ? "Show Less" : "Read More"}
+                      </Button>
+                    </div>
+                  </div>
+                </Card>
+              </Box>
             </div>
           </div>
         </Box>
 
-        <PopularNewCars popularVehicles={popularVehicles} type={vehicleType}/>
+        <PopularNewCars popularVehicles={popularVehicles} type={vehicleType} />
 
-        <UpcomingCars fetchUpComingVehicles={fetchUpComingVehicles} type={vehicleType}/>
-        <ComparisonProducts type={vehicleType}/>
-        <BrowseVideos type={vehicleType}/>
+        <UpcomingCars
+          fetchUpComingVehicles={fetchUpComingVehicles}
+          type={vehicleType}
+        />
+        <ComparisonProducts type={vehicleType} />
+        <BrowseVideos type={vehicleType} />
         <BrowseBlogs />
 
         <Comments
@@ -196,14 +215,18 @@ const MakesVehicles = ({
                           justify="center"
                           align="center"
                           className="single-brand-item"
-                        >                       
+                        >
                           <NextImage
                             width={100}
                             height={100}
                             src={item.companyImage}
                             className="mx-auto text-center"
                           />
-                          <Anchor component={Link} href={`/new/car/make/${item.name}`} c="#333">
+                          <Anchor
+                            component={Link}
+                            href={`/new/car/make/${item.name}`}
+                            c="#333"
+                          >
                             {item.name}
                           </Anchor>
                         </Flex>
