8a9c3f81d26138bc17c1f1803093aa303f703786
shanmaqbool <shanmaqbool12345@gmail.com>
2025-03-15 22:07:16 +0500

    condoton works

--- modules/blogs/FollowUs.jsx
+++ modules/blogs/FollowUs.jsx
@@ -1,37 +1,8 @@
 "use client"
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Card, Grid, Stack, Text, Title } from '@mantine/core';
 import { FacebookCircle, YoutubeCircle, InstagramCircle } from "@/components/Icons";
-import { fetchAPI } from '@/services/fetchAPI';
 
-
-const YOUTUBE_API_KEY = "AIzaSyA6wG9RIgXNTN9Xs5_GxtFJUFW12pXmPYE";
-const CHANNEL_ID = "UCgtwk5-YCUZfhQEdbfwdLYA";
-
-
-
-
-const FollowUs = () => {
-  const [subscribers,setSubscribers] =useState()
-
-   const getYouTubeStats = async () => {
-    try {
-      const response = await fetchAPI(
-        `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${CHANNEL_ID}&key=${YOUTUBE_API_KEY}`
-      );
-      setSubscribers(response.items[0]?.statistics?.subscriberCount)
-    } catch (error) {
-      console.error('Error fetching YouTube stats:', error);
-      return 0;
-    }
-  };
-  
-  useEffect(() => {
-    getYouTubeStats()
-  }, [])
-
-
-  
 const socialMediaStats = [
   {
     icon: FacebookCircle,
@@ -40,7 +11,7 @@ const socialMediaStats = [
   },
   {
     icon: YoutubeCircle,
-    count: subscribers,
+    count: "20,100",
     label: "Subscribers",
   },
   {
@@ -49,7 +20,8 @@ const socialMediaStats = [
     label: "Followers",
   },
 ];
-  
+
+const FollowUs = () => {
   return (
     <Card
       shadow="0px 4px 20px 0px #00000014"
@@ -87,3 +59,103 @@ const socialMediaStats = [
 };
 
 export default FollowUs;
+
+
+
+
+
+
+
+
+
+
+
+// "use client"
+// import React, { useEffect, useState } from 'react';
+// import { Card, Grid, Stack, Text, Title } from '@mantine/core';
+// import { FacebookCircle, YoutubeCircle, InstagramCircle } from "@/components/Icons";
+// import { fetchAPI } from '@/services/fetchAPI';
+
+
+// const YOUTUBE_API_KEY = "AIzaSyA6wG9RIgXNTN9Xs5_GxtFJUFW12pXmPYE";
+// const CHANNEL_ID = "UCgtwk5-YCUZfhQEdbfwdLYA";
+
+
+
+
+// const FollowUs = () => {
+//   const [subscribers,setSubscribers] =useState()
+
+//    const getYouTubeStats = async () => {
+//     try {
+//       const response = await fetchAPI(
+//         `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${CHANNEL_ID}&key=${YOUTUBE_API_KEY}`
+//       );
+//       setSubscribers(response.items[0]?.statistics?.subscriberCount)
+//     } catch (error) {
+//       console.error('Error fetching YouTube stats:', error);
+//       return 0;
+//     }
+//   };
+  
+//   useEffect(() => {
+//     getYouTubeStats()
+//   }, [])
+
+
+  
+// const socialMediaStats = [
+//   {
+//     icon: FacebookCircle,
+//     count: 563,
+//     label: "Fans",
+//   },
+//   {
+//     icon: YoutubeCircle,
+//     count: subscribers,
+//     label: "Subscribers",
+//   },
+//   {
+//     icon: InstagramCircle,
+//     count: 563,
+//     label: "Followers",
+//   },
+// ];
+  
+//   return (
+//     <Card
+//       shadow="0px 4px 20px 0px #00000014"
+//       padding="md"
+//       mb="lg"
+//       withBorder
+//       className="border-light"
+//     >
+//       <Title
+//         order={5}
+//         className="title-with-border"
+//         pb="xs"
+//         mb="xs"
+//         fz="14px"
+//       >
+//         Follow Us
+//       </Title>
+//       <Grid mt="md">
+//         {socialMediaStats.map(({ icon: Icon, count, label }, index) => (
+//           <Grid.Col key={index} span={4}>
+//             <Stack align="center" justify="center" gap="0">
+//               <Icon />
+//               <Text fz="12px" fw={700} mt="xs" c="#222222">
+//                 {count}
+//               </Text>
+//               <Text fz="12px" size="sm" c="#222222">
+//                 {label}
+//               </Text>
+//             </Stack>
+//           </Grid.Col>
+//         ))}
+//       </Grid>
+//     </Card>
+//   );
+// };
+
+// export default FollowUs;
