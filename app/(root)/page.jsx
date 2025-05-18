301bfca0af82edd5ddae7bd93730ce99c792231a
shanmaqbool <shanmaqbool12345@gmail.com>
2025-05-18 10:53:31 +0500

    Import HomeModule in page.jsx to enable home functionality
    - Added the HomeModule import to the page component for improved integration.
    - Ensured the component is ready for further development and user experience enhancements.

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,5 +1,5 @@
-// import HomeModule from "@/modules/home";
 "use client"
+import HomeModule from "@/modules/home";
 import LoadingWrapper from "@/components/loading-wrapper";
 import { Suspense } from 'react';
 
